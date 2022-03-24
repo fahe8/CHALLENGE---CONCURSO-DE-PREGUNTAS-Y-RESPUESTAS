33fe9ee64029ee8342f73874951c9e2dc49756db
Fabián Carrión <fabiancarrion1@gmail.com>
2022-03-23 20:33:28 -0500

    html con estilos basicos y clases para las entidades

---
+++ app.js
@@ -0,0 +1,18 @@
+import { Player } from "./modulos/Player.js";
+import { Round } from "../modulos/Round.js";
+import { Questions } from "./modulos/Questions.js";
+import { arrayQuestions } from "./Data/data.js";
+import { Game } from "./modulos/Game.js";
+
+const respuestas = document.getElementById('containerAnswer');
+const preguntahtml = document.getElementById('pregunta');
+const cambiarRespuestas = document.querySelectorAll('.answer')
+const roundhtml = document.getElementById('round');
+const table = document.getElementById('table')
+const exit = document.getElementById('exit')
+
+
+
+
+
+
